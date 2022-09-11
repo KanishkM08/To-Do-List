b9abef8482fd83b067a134a7d1295447f5ca45f8
KanishkM08 <malhotra.kanishk0803@gmail.com>
2022-09-11 22:17:23 +0530

    fixed some stupid bugs

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-document.querySelector('#push').onClick  = function() {
-    if(document.querySelector('#taskInput').value.length == 0){
-        alert("Please enter a task!")
+document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#taskInput').value.length == 0) {
+        alert("Please Enter a Task")
     }
-    else{
+    else {
         document.querySelector('#tasks').innerHTML += `
-    <div class = "task">
-        <span id = "taskname">
+    <div class="task">
+        <span id="taskname">
             ${document.querySelector('#taskInput').value}
         </span>
-        <button class = "delete">
-            <i class = "far fa-trash alt"></i>
+        <button class="delete">
+            <i class="far fa-trash-alt"></i>
         </button>
     </div>
 `;
+
         var current_tasks = document.querySelectorAll(".delete");
-        for(var i = 0 < current_tasks.length; i++;){
-            current_tasks[i].onClick = function(){
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
                 this.parentNode.remove();
             }
         }
